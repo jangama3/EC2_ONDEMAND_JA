ac9de7c97a5ffb2b35216628bc44d2145f90223b
Jonathan Angamarca <49312416+jangama3@users.noreply.github.com>
2025-11-05 16:05:08 -0500

    fix duplicate

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    local   = {
-      source  = "hashicorp/local"
-      version = ">= 2.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.0"
-    }
-  }
+  #required_providers {
+  #  aws = {
+   #   source  = "hashicorp/aws"
+    #  version = ">= 5.0"
+   # }
+    #local   = {
+     # source  = "hashicorp/local"
+      #version = ">= 2.0"
+    #}
+    #archive = {
+     # source  = "hashicorp/archive"
+      #version = ">= 2.0"
+   # }
+ # }
 }
 
 variable "region" {
