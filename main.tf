d7fa0b8ca06733110775597af1324297f16f74d3
Jonathan Angamarca <49312416+jangama3@users.noreply.github.com>
2025-11-05 16:12:05 -0500

    fix main

--- main.tf
+++ main.tf
@@ -1,103 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+############################################
+# 1) Generate Lambda Source File
+############################################
 resource "local_file" "lambda_src" {
   filename = "lambda_function.py"
-  content  = <<-PY
-import json
-import boto3
-import os
 
-ec2 = boto3.client("ec2")
+  content = <<-EOT
+    import json
+    import boto3
 
-TAG_KEY = "Environment"
-TAG_VALUE = "Dev"
+    ec2 = boto3.client("ec2")
 
-def list_instances():
-    resp = ec2.describe_instances(
-        Filters=[{"Name": f"tag:{TAG_KEY}", "Values": [TAG_VALUE]}]
-    )
-    instances = []
-    for r in resp["Reservations"]:
-        for i in r["Instances"]:
-            name = next((t["Value"] for t in i.get("Tags", []) if t.get("Key") == "Name"), i.get("InstanceId"))
-            instances.append({"id": i["InstanceId"], "name": name, "state": i["State"]["Name"]})
-    return instances
+    TAG_KEY = "Environment"
+    TAG_VALUE = "Dev"
 
-def get_instance(instance_id):
-    resp = ec2.describe_instances(InstanceIds=[instance_id])
-    for r in resp["Reservations"]:
-        for i in r["Instances"]:
-            return i
-    return None
+    def list_instances():
+        resp = ec2.describe_instances(
+            Filters=[{"Name": f"tag:{TAG_KEY}", "Values": [TAG_VALUE]}]
+        )
+        instances = []
+        for r in resp["Reservations"]:
+            for i in r["Instances"]:
+                name = next((t["Value"] for t in i.get("Tags", []) if t.get("Key") == "Name"), i.get("InstanceId"))
+                instances.append({"id": i["InstanceId"], "name": name, "state": i["State"]["Name"]})
+        return instances
 
-def lambda_handler(event, context):
-    print("EVENT:", json.dumps(event))
+    def lambda_handler(event, context):
+        action = event.get("action")
+        instance_id = event.get("instance_id")
 
-    action = event.get("action")
-    instance_id = event.get("instance_id")
+        if action == "list":
+            return {"statusCode": 200, "body": json.dumps(list_instances())}
 
-    if action == "list":
-        return {"statusCode": 200, "body": json.dumps(list_instances())}
+        if not instance_id:
+            return {"statusCode": 400, "body": json.dumps({"error": "No instance id provided"})}
 
-    if not instance_id:
-        return {"statusCode": 400, "body": json.dumps({"error": "No instance id provided"})}
+        if action == "start":
+            ec2.start_instances(InstanceIds=[instance_id])
+            return {"statusCode": 200, "body": json.dumps({"message": f"Started {instance_id}"})}
 
-    if action == "start":
-        ec2.start_instances(InstanceIds=[instance_id])
-        return {"statusCode": 200, "body": json.dumps({"message": f"Started {instance_id}"})}
+        if action == "stop":
+            ec2.stop_instances(InstanceIds=[instance_id])
+            return {"statusCode": 200, "body": json.dumps({"message": f"Stopped {instance_id}"})}
 
-    if action == "stop":
-        ec2.stop_instances(InstanceIds=[instance_id])
-        return {"statusCode": 200, "body": json.dumps({"message": f"Stopped {instance_id}"})}
-
-    return {"statusCode": 400, "body": json.dumps({"error": "Invalid action"})}
-PY
+        return {"statusCode": 400, "body": json.dumps({"error": "Invalid action"})}
+  EOT
 }
 
-# --- Lambda Role ---
+############################################
+# 2) Zip the Lambda
+############################################
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = local_file.lambda_src.filename
+  output_path = "lambda_function.zip"
+}
+
+############################################
+# 3) IAM Role + Permissions
+############################################
 resource "aws_iam_role" "lambda_role" {
-  name = "ec2_control_lambda_role"
+  name = "EC2_Control_Lambda_Role"
 
-  assume_role_policy = <<-EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Effect": "Allow",
-    "Principal": {"Service": "lambda.amazonaws.com"},
-    "Action": "sts:AssumeRole"
-  }]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
-  name = "ec2_control_policy"
+  name = "EC2_Control_Lambda_Policy"
   role = aws_iam_role.lambda_role.id
 
-  policy = <<-EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Effect": "Allow",
-    "Action": [
-      "ec2:StartInstances",
-      "ec2:StopInstances",
-      "ec2:DescribeInstances"
-    ],
-    "Resource": "*"
-  }]
-}
-EOF
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ec2:DescribeInstances",
+        "ec2:StartInstances",
+        "ec2:StopInstances"
+      ]
+      Resource = "*"
+    }]
+  })
 }
 
-# --- Lambda Function ---
+############################################
+# 4) Lambda Function
+############################################
 resource "aws_lambda_function" "ec2_control" {
   function_name = "EC2_Control_Lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
-  filename      = local_file.lambda_src.filename
+  filename      = data.archive_file.lambda_zip.output_path
+  timeout       = 5
 }
 
-# --- API Gateway ---
+############################################
+# 5) API Gateway HTTP API
+############################################
 resource "aws_apigatewayv2_api" "api" {
   name          = "EC2ControlAPI"
   protocol_type = "HTTP"
@@ -116,7 +141,7 @@ resource "aws_apigatewayv2_route" "route" {
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-resource "aws_lambda_permission" "api_lambda_permission" {
+resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ec2_control.function_name
@@ -124,6 +149,9 @@ resource "aws_lambda_permission" "api_lambda_permission" {
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
-output "api_url" {
+############################################
+# 6) Output API Endpoint
+############################################
+output "api_endpoint" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
