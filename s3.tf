196c711487f9562c56bf6c24893a88d7fbd1c684
Jonathan Angamarca <49312416+jangama3@users.noreply.github.com>
2025-11-06 13:30:21 -0500

    fix policy

--- s3.tf
+++ s3.tf
@@ -1,16 +1,14 @@
 
-# Create the S3 bucket
 resource "aws_s3_bucket" "static_website" {
-  bucket        = var.bucketname
-  force_destroy = true
+  bucket = var.bucket_name
 
   tags = {
-    Name        = var.bucketname
-    Environment = "prod"
+    Name        = "StaticWebsiteTerraform"
+    Environment = "Dev"
   }
 }
 
-# Enforce bucket ownership
+# Ensure bucket ownership
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.static_website.id
 
@@ -19,7 +17,35 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
   }
 }
 
-# Configure bucket for static website hosting
+# Configure public access block to allow public objects
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Bucket policy to allow public read for objects
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
+# Configure S3 for website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.static_website.id
 
--- variables.tf
+++ variables.tf
@@ -42,6 +42,6 @@ variable "lambda_name" {
 variable "bucketname" {
   description = "The name of the S3 bucket for the static website"
   type        = string
-  default     = "2ec2ondemandja2625-unique12345"
+  default     = "22ec2ondemandja2625-unique12345"
 }
 
